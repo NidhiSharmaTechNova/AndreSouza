41500bf4608a6b1517509ffd84d7b8210ebef3c8
Saloni Sharma <saloni94666@gmail.com>
2026-04-06 16:13:17 +0530

    fix code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 
 import BottomNav from "./Components/BottomNav/BottomNav"
 import Home from "./pages/Home/Home"
